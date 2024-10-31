cae12269e8e4e6ccfb43dcd060a6cda5a299129d
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2024-10-30 22:21:16 -0300

    adocoptado para tente de th

---
+++ autenticacao/src/main/java/gustavo/stateless/autenticacao/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package gustavo.stateless.autenticacao.repository;
+
+import gustavo.stateless.autenticacao.core.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    Optional<User> findByUsername(String username);
+
+}
